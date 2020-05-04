306b40c3c6d6168ca363535450cdf27f85142b7e
ian-thomas-41 <60229585+ian-thomas-41@users.noreply.github.com>
2020-05-04 12:16:45 -0600

    urls, views, admin.py changes
    now when we do makemigrations it says no changes detected

--- cadettracker/admin.py
+++ cadettracker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Cadet, Company, Locations
+from .models import Company, Location
 
-admin.site.register(Cadet)
+#admin.site.register(Cadet)
 admin.site.register(Company)
