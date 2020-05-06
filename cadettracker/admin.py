c4f5161c6c42fddd1441b768275a52d558cc9382
nathaniellambert <60356150+nathaniellambert@users.noreply.github.com>
2020-05-05 22:49:06 -0400

    Adding to models.

--- cadettracker/admin.py
+++ cadettracker/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Company, Location, Supply
+from .models import Company, Location, Supply, Personnel, Regiment, CompanyHasSupply, RegHasSupply
 
-#admin.site.register(Cadet)
 admin.site.register(Company)
+admin.site.register(Location)
+admin.site.register(Supply)
+admin.site.register(Personnel)
+admin.site.register(Regiment)
+admin.site.register(CompanyHasSupply)
+admin.site.register(RegHasSupply)
--- cadettracker/urls.py
+++ cadettracker/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+
 
 from . import views
 
 app_name='cadettracker'
 urlpatterns = [
     #path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
