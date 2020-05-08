08184dc9679eb6392e5f988325576f25cc5dcbea
nathaniellambert <60356150+nathaniellambert@users.noreply.github.com>
2020-05-08 16:51:22 -0400

    Debug off

--- classproject2/settings.py
+++ classproject2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ans9fo_2qu@)k)e#pawjqn*i!!gkxnyy2x35&mc$^ruj1&9#!o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
