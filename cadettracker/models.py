67db4d689df45850952cca655b8c7ed8c39de069
nathaniellambert <60356150+nathaniellambert@users.noreply.github.com>
2020-05-07 21:22:53 -0400

    Recreating model

--- cadettracker/models.py
+++ cadettracker/models.py
@@ -68,7 +68,7 @@ class Personnel(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     phoneNum = models.CharField(max_length=10)
     #companyLabel = models.CharField(max_length = 2)
-    regimentLabel = models.ForeignKey(Regiment, on_delete=models.CASCADE)
+    #regimentLabel = models.ForeignKey(Regiment, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 # class CompanyHasPersonnel(models.Model):
